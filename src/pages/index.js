17b1dbf4c7504e2c15e432fbb669a17d9fdcd06a
Michelle Fang <michelle@coinlist.co>
2023-06-02 19:20:19 -0400

    fix ipad canvas size

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,6 +25,10 @@ export default function Home() {
       <Head>
         <title>Text to 3D with Primitive Shapes</title>
         <link rel="icon" href="/genie2.png" />
+        <meta
+          name="viewport"
+          content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=no"
+        ></meta>
       </Head>
       <Dashboard canvasSize={canvasSize} />
     </div>
